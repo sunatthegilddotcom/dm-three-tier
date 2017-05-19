d62df5f6f52f285a0a3076e2e9cdbc480db3c1f4
Rob Horner <robert@horners.org>
2017-05-19 17:38:44 -0600

    read "color" attribute to change text color

--- web-startup-script.sh
+++ web-startup-script.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 apt-get update
 apt-get install -y apache2
-INSTANCE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/hostname -H "Metadata-Flavor: Google")
-IPADDR=$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip -H "Metadata-Flavor: Google")
+INSTANCE=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/hostname)
+IPADDR=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+COLOR=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/color)
 cat <<EOF > /var/www/html/index.html
 <html><body><h2>Hello World</h2>
-<h2>I'm running on instance $INSTANCE</h2>
+<h2 style="background-color:$COLOR; color:white">I'm running on instance $INSTANCE</h2>
 <h2>My internal IP address is $IPADDR</h2>
 </body></html>
 EOF
